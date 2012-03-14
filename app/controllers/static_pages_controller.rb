22570d20c80d23aedd5c03caef5a2b85960011eb
Charles Oppenheimer <choppen5@yahoo.com>
2012-03-14 11:30:08 -0700

    changed to work with new session except sorting

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,14 +5,18 @@ class StaticPagesController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(:page => params[:page])
       @companies_to_post_to ||= []
-      @companies_to_post_to = session[:company_ids].map{|c| Company.find(c)}
+      if session[:company_ids]
+        @companies_to_post_to = session[:company_ids].map{|c| Company.find(c)}
+      end 
     end
   end
 
   def add_company_to_homepage
     session[:company_ids] ||= []
     session[:company_ids] << params[:company_id]
-    session[:company_ids].uniq!.sort
+    
+      #session[:company_ids].uniq!.sort
+      
     redirect_to root_path
   end
 
